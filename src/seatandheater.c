bbe5aafd303cc3d79eb75e786b5749ef31077dd3
Prasadpokanati <82254441+Prasadpokanati@users.noreply.github.com>
2021-04-30 20:03:00 +0530

    Add files via upload

---
+++ src/seatandheater.c
@@ -0,0 +1,16 @@
+#include <avr/io.h>
+#include "seatandheater.h"
+
+/**
+ * @brief function for declaring peripherals led,button,heater.
+ *
+ */
+void peripheral_init(){
+SET_LED_AS_OUTPUT;
+ SET_BUTTON_AS_INPUT;
+ SET_HEATER_AS_INPUT;
+ PULL_UP_BUTTON;
+ PULL_UP_HEATER ;
+
+
+}
